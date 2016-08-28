ea8abbf82a41966709f1ece4b252f66cbcf24096
David Amanshia <phpengine@pharaohtools.com>
2016-08-28 15:15:53 +0100

    oauth updated configuration settings

--- src/Modules/OAuth/info.OAuth.php
+++ src/Modules/OAuth/info.OAuth.php
@@ -23,26 +23,24 @@ class OAuthInfo extends PTConfigureBase {
 	public function configuration() {
 
 		return array(
-				"google" => array("type" => "", "default" => "", "label" => "Google Oauth Credentials", ),
-				"google_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ),
-				"google_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ),
-				"google_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=googlelogin", "label" => "Redirect URI", ),
-				
-				"git" => array("type" => "", "default" => "", "label" => "Git-hub Oauth Credentials", ),
-				"git_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ), 
-				"git_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ), 
-				"git_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=githublogin", "label" => "Redirect URI", ), 
-				
-				"fb" => array("type" => "", "default" => "", "label" => "Facebook Oauth Credentials", ), 
-				"fb_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ), 
-				"fb_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ), 
-				"fb_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=fblogin", "label" => "Redirect URI", ), 
-				
-				"li" => array("type" => "", "default" => "", "label" => "Linked-In Oauth Credentials", ), 
-				"li_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ), 
-				"li_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ), 
-				"li_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=linkedinlogin", "label" => "Redirect URI", ),
-				 );
+            "oauth_enabled" => array("type" => "boolean", "default" => "", "label" => "Enable Login by Oauth Providers", ),
+            "google_enabled" => array("type" => "boolean", "default" => "", "label" => "Enable Login by Google Oauth Credentials", ),
+            "google_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ),
+            "google_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ),
+            "google_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=googlelogin", "label" => "Redirect URI", ),
+            "github_enabled" => array("type" => "boolean", "default" => "", "label" => "Enable Login by Github Oauth Credentials", ),
+            "github_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ),
+            "github_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ),
+            "github_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=githublogin", "label" => "Redirect URI", ),
+            "fb_enabled" => array("type" => "boolean", "default" => "", "label" => "Enable Login by Facebook Oauth Credentials", ),
+            "fb_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ),
+            "fb_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ),
+            "fb_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=fblogin", "label" => "Redirect URI", ),
+            "li_enabled" => array("type" => "boolean", "default" => "", "label" => "Enable Login by Linked-In Oauth Credentials", ),
+            "li_client_id" => array("type" => "text", "default" => "client id", "label" => "Client ID", ),
+            "li_client_secret" => array("type" => "text", "default" => "client secret", "label" => "Client Secret", ),
+            "li_redirect_uri" => array("type" => "text", "default" => "http://www.example.com/index.php?control=OAuth&action=linkedinlogin", "label" => "Redirect URI", ),
+         );
 	}
 
 	public function helpDefinition() {
