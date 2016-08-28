6922d577a0fef700158823e256143bfb95b0e36c
David Amanshia <phpengine@pharaohtools.com>
2016-08-28 15:13:45 +0100

    build histories for build home page

--- src/Modules/BuildHome/Model/BuildHomeAllOS.php
+++ src/Modules/BuildHome/Model/BuildHomeAllOS.php
@@ -17,6 +17,7 @@ class BuildHomeAllOS extends Base {
     public function getData() {
         $ret["pipeline"] = $this->getPipeline();
         $ret["features"] = $this->getPipelineFeatures();
+        $ret["historic_builds"] = $this->getOldBuilds();
         return $ret ;
     }
 
@@ -30,6 +31,19 @@ class BuildHomeAllOS extends Base {
         $pipeline = $pipelineFactory->getModel($this->params);
         $r = $pipeline->getPipeline($this->params["item"]);
         return $r ;
+    }
+
+    private function getOldBuilds() {
+        $pdir = PIPEDIR.DS.$this->params["item"].DS.'history' ;
+        $builds = scandir($pdir) ;
+        $buildsRay = array();
+        $limit = count($builds) ;
+        $limit = ($limit < 10) ? $limit : 1 ;
+        for ($i=0; $i < $limit; $i++) {
+            if (!in_array($builds[$i], array(".", "..", "tmpfile"))){
+                $buildsRay[] = $builds[$i] ; } }
+        rsort($buildsRay) ;
+        return $buildsRay ;
     }
 
     public function getPipelineFeatures() {
