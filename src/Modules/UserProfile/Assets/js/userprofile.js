4a5ad94abc7d8f6fea2c6fb9179bac84cd54cd99
David Amanshia <phpengine@pharaohtools.com>
2016-08-29 02:23:38 +0100

    delete and update user js

--- src/Modules/UserProfile/Assets/js/userprofile.js
+++ src/Modules/UserProfile/Assets/js/userprofile.js
@@ -13,22 +13,22 @@ function changePassword() {
     $('#password_match_error').html('');
 
     if ($('#update_password').val() == '') {
-        $('#update_password_alert').html('&nbsp;&nbsp;Please enter your old password');
+        $('#update_password_alert').html('Please enter your password');
         $('#update_password').focus();
         return; }
 
     if ($('#update_password_match').val() == '') {
-        $('#update_password_alert').html('&nbsp;&nbsp;Please enter your new password');
+        $('#update_password_alert').html('Please enter your new password');
         $('#update_password_match').focus();
         return; }
 
     if ($('#update_password_match').val() == '') {
-        $('#update_password_match_alert').html('&nbsp;&nbsp;Please enter new password Again');
+        $('#update_password_match_alert').html('Please enter new password Again');
         $('#update_password_match').focus();
         return; }
 
     if ($('#update_password').val() != $('#update_password_match').val()) {
-        $('#password_match_error').html('&nbsp;&nbsp;Password Does Not Match...Try Again');
+        $('#password_match_error').html('Password Does Not Match...Try Again');
         $('#update_password_match').val('');
         $('#update_password_match').focus();
         return; }
@@ -55,16 +55,6 @@ function changePassword() {
 function createUser() {
     rurl = $('#base_url').val() + '/index.php?control=UserProfile&action=create&output-format=JSON' ;
     console.log(rurl) ;
-
-//    $('#update_password').html('');
-//    $('#update_password_match').html('');
-//    $('#update_password_match').html('');
-//
-//    $('#update_password_alert').html('');
-//    $('#update_password_alert').html('');
-//    $('#update_password_match_alert').html('');
-//
-//    $('#password_match_error').html('');
 
     if ($('#create_username').val() == '') {
         $('#create_username_alert').html('Please enter a Username');
@@ -116,13 +106,13 @@ function createUser() {
         dataType: "json",
         success: function(result) { console.log(result);
 
-            if (result.status == true) {
+            if (result.data.status == true) {
                 $('#form_alert').addClass('successMessage');
-                $('#form_alert').html(result.message);
+                $('#form_alert').html(result.data.message);
                 $('#form_alert').focus(); }
             else {
                 $('#form_alert').addClass('errorMessage');
-                $('#form_alert').html(result.message);
+                $('#form_alert').html(result.data.message);
                 $('#form_alert').focus(); } },
         error: function(result, textStatus, errorThrown) { console.log(result);
             $('#password_error_msg').html(''+textStatus+' '+errorThrown+' '+result.msg);
@@ -131,12 +121,107 @@ function createUser() {
     });
 }
 
+function deleteUser() {
+
+    if (confirm("Are you Sure about deleting this User?") == true) {
+
+        $('#update_email').val("Loading...") ;
+        $('#update_username_text').val("Loading...") ;
+//        $('#update_username_text').val("Loading...") ;
+        uname = $('#update_username').val() ;
+
+        rurl = $('#base_url').val() + '/index.php?control=UserProfile&action=delete&output-format=JSON' ;
+        console.log(rurl) ;
+
+        $.ajax({
+            type: 'POST',
+            url: rurl,
+            data: {
+                create_username: uname
+            },
+            dataType: "json",
+            success: function(result) {
+                console.log(result);
+                if (result.data.status == true) {
+                    $('#form_alert').addClass('successMessage');
+                    $('#form_alert').html(result.data.message);
+                    $('#form_alert').focus(); }
+                else {
+                    $('#form_alert').addClass('errorMessage');
+                    $('#form_alert').html(result.data.message);
+                    $('#form_alert').focus(); } },
+            error: function(result, textStatus, errorThrown) { console.log(result);
+                $('#password_error_msg').html(''+textStatus+' '+errorThrown+' '+result.msg);
+                $('#password_error_msg').focus(); }
+
+        });
+
+    }
+}
+
+function updateUser() {
+
+    if (confirm("Are you Sure about updating this User?") == true) {
+        uname = $('#update_username').val() ;
+
+        $('#update_password_alert').html('');
+        $('#update_password_match_alert').html('');
+
+        if ($('#update_password').val() == '') {
+            $('#update_password_alert').html('Please enter your password');
+            $('#update_password').focus();
+            return; }
+
+        if ($('#update_password_match').val() == '') {
+            $('#update_password_match_alert').html('Please enter your new password');
+            $('#update_password_match').focus();
+            return; }
+
+        if ($('#update_password_match').val() == '') {
+            $('#update_password_match_alert').html('Please enter new password Again');
+            $('#update_password_match').focus();
+            return; }
+
+        if ($('#update_password').val() != $('#update_password_match').val()) {
+            $('#update_password_match_alert').html('Passwords do not match...Try Again');
+            $('#update_password_match').val('');
+            $('#update_password_match').focus();
+            return; }
+
+        rurl = $('#base_url').val() + '/index.php?control=UserProfile&action=changepassword&output-format=JSON' ;
+        console.log(rurl) ;
+
+        $.ajax({
+            type: 'POST',
+            url: rurl,
+            data: {
+                create_username: uname
+            },
+            dataType: "json",
+            success: function(result) {
+                console.log(result);
+                if (result.status == true) {
+                    $('#form_alert').addClass('successMessage');
+                    $('#form_alert').html(result.message);
+                    $('#form_alert').focus(); }
+                else {
+                    $('#form_alert').addClass('errorMessage');
+                    $('#form_alert').html(result.message);
+                    $('#form_alert').focus(); } },
+            error: function(result, textStatus, errorThrown) { console.log(result);
+                $('#password_error_msg').html(''+textStatus+' '+errorThrown+' '+result.msg);
+                $('#password_error_msg').focus(); }
+
+        });
+
+    }
+}
+
 
 function refreshUserDetails(username) {
 
     $('#update_email').val("Loading...") ;
     $('#update_username_text').val("Loading...") ;
-    $('#update_username').val("Loading...") ;
 
     rurl = $('#base_url').val() + '/index.php?control=UserProfile&action=get-user&output-format=JSON' ;
     console.log(rurl) ;
@@ -174,6 +259,9 @@ function updateUserFields(userData) {
     $('#update_username').val(userData.username) ;
     btn_str  = '<button onclick="updateUser(); return false;" class="btn btn-success hvr-grow-shadow actionButton">' ;
     btn_str += '  Update Details ' ;
+    btn_str += '</button>' ;
+    btn_str += '<button onclick="deleteUser(); return false;" class="btn btn-warning hvr-grow-shadow actionButton">' ;
+    btn_str += '  Delete User ' ;
     btn_str += '</button>' ;
     $('.actionButtonWrap').html(btn_str) ;
 }
