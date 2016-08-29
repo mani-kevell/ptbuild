cc343ca694e6323db852f0192d3e99dde64357f5
David Amanshia <phpengine@pharaohtools.com>
2016-08-29 16:23:55 +0100

    starting to integrate email sending with new user creation

--- src/Modules/UserProfile/Model/UserProfileAnyOS.php
+++ src/Modules/UserProfile/Model/UserProfileAnyOS.php
@@ -21,17 +21,18 @@ class UserProfileAnyOS extends BasePHPApp {
     public function getData() {
         $ret['user'] = $this->getUserDetails();
         $ret['allusers'] = $this->getAllUserDetails();
+        $ret['email_users_enabled'] = $this->getEnabledStatus();
         return $ret ;
     }
 
     public function saveData() {
         $user = new \stdClass() ;
         // @todo sanitize these request vars. Use Params?
-        $user->username = $_REQUEST['update_username'];
-        $user->email = $_REQUEST['update_email'];
-        if (isset($_REQUEST['update_password']) &&
-            ($_REQUEST['update_password'] == $_REQUEST['update_password_match'])) {
-            $user->password = $_REQUEST['update_password']; }
+        $user->username = $this->params['update_username'];
+        $user->email = $this->params['update_email'];
+        if (isset($this->params['update_password']) &&
+            ($this->params['update_password'] == $this->params['update_password_match'])) {
+            $user->password = $this->params['update_password']; }
         $this->saveUser($user);
     }
 
@@ -40,6 +41,17 @@ class UserProfileAnyOS extends BasePHPApp {
         $signup = $signupFactory->getModel($this->params);
         $oldData=$signup->getLoggedInUserData();
         return $oldData;
+    }
+
+    public function getEnabledStatus() {
+        $signupFactory = new \Model\Signup();
+        $signup = $signupFactory->getModel($this->params);
+        $me = $signup->getLoggedInUserData() ;
+        $rid = $signup->getUserRole($me->email);
+        if ($rid == 1) {
+            $au =$signup->getUsersData();
+            return $au;  }
+        return false ;
     }
 
     public function getAllUserDetails() {
