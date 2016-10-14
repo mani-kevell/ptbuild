c27bd84c320cc4b4497ca84d48ed00660da15e96
David Amanshia <phpengine@pharaohtools.com>
2016-10-14 02:30:03 +0100

    added PHARAOH_APP_FRIENDLY to constants

--- src/Constants.php
+++ src/Constants.php
@@ -5,6 +5,7 @@
  */
 
 define('PHARAOH_APP', "ptbuild") ;
+define('PHARAOH_APP_FRIENDLY', str_replace("pt", "", PHARAOH_APP)) ;
 
 if (in_array(PHP_OS, array("Windows", "WINNT"))) {
     $sd = getenv('SystemDrive') ;
