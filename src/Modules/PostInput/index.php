0c0f65b85aa4ca84f68d3b28df5dba62e7b7abe3
David Amanshia <phpengine@pharaohtools.com>
2016-11-11 09:56:20 +0000

    check force ssl exists before using it

--- src/Modules/PostInput/index.php
+++ src/Modules/PostInput/index.php
@@ -8,7 +8,8 @@
 require_once(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR."Constants.php");
 $vardata = file_get_contents(PFILESDIR.PHARAOH_APP.DS.PHARAOH_APP.DS.PHARAOH_APP.'vars') ;
 $vars = json_decode($vardata, true);
-$fsslval = $vars["mod_config"]["ApplicationInstance"]["force_ssl"] ;
+$fsslval = (isset($vars["mod_config"]["ApplicationInstance"]["force_ssl"])) ?
+    $vars["mod_config"]["ApplicationInstance"]["force_ssl"] : "" ;
 $force_ssl = (isset($fsslval) && $fsslval=="on") ? true : false ;
 if ($force_ssl == true) {
     if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
