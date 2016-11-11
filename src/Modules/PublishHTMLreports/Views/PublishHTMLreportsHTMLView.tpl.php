14420646e42bf919e8f9ad04f00b5e2eefad39df
David Amanshia <phpengine@pharaohtools.com>
2016-11-11 04:23:58 +0000

    page layout of html reports

--- src/Modules/PublishHTMLreports/Views/PublishHTMLreportsHTMLView.tpl.php
+++ src/Modules/PublishHTMLreports/Views/PublishHTMLreportsHTMLView.tpl.php
@@ -1,17 +1,24 @@
 <div class="container" id="wrapper">
     <div class="col-lg-9">
-        <h3>
-        <?php
-            echo $pageVars["data"]["current_report"]["data"]["Report_Title"] ;
-        ?>
-        </h3>
-        <h2> Report: </h2>
-        <?php
-            echo $pageVars["data"]["current_report"]["report_data"] ;
-        ?>
+        <div class="fullRow">
+            <h2>
+                <?php
+                    echo $pageVars["data"]["current_report"]["feature_data"]["Report_Title"] ;
+                ?>
+            </h2>
+        </div>
+        <div class="fullRow">
+            <hr />
+            <h4> Report: </h4>
+            <?php
+                echo $pageVars["data"]["current_report"]["report_data"] ;
+            ?>
+        </div>
+        <div class="fullRow">
+            <hr />
+            <p class="text-center">
+                Visit <a href="http://www.pharaohtools.com">www.pharaohtools.com</a> for more
+            </p>
+        </div>
     </div>
-    <hr>
-    <p class="text-center">
-        Visit <a href="http://www.pharaohtools.com">www.pharaohtools.com</a> for more
-    </p>
 </div>
