42624a5d4e21f84d6b66a0d97ebdf3e071262d6d
David Amanshia <phpengine@pharaohtools.com>
2016-11-11 03:19:01 +0000

    first draft of list view for html reports

---
+++ src/Modules/PublishHTMLreports/Views/PublishHTMLreportsListHTMLView.tpl.php
@@ -0,0 +1,14 @@
+<div class="container" id="wrapper">
+    <div class="col-lg-9">
+        <?php
+
+            var_dump( $pageVars["data"] ) ;
+            echo $pageVars["data"]["report_list"] ;
+
+        ?>
+    </div>
+    <hr>
+    <p class="text-center">
+        Visit <a href="http://www.pharaohtools.com">www.pharaohtools.com</a> for more
+    </p>
+</div>
