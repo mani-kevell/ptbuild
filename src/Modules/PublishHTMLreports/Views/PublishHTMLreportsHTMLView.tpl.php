297541764ede28b52602e858654e15f045d1aa58
David Amanshia <phpengine@pharaohtools.com>
2016-11-11 06:59:44 +0000

    report displays correctly

--- src/Modules/PublishHTMLreports/Views/PublishHTMLreportsHTMLView.tpl.php
+++ src/Modules/PublishHTMLreports/Views/PublishHTMLreportsHTMLView.tpl.php
@@ -1,24 +1,22 @@
 <div class="container" id="wrapper">
-    <div class="col-lg-9">
-        <div class="fullRow">
-            <h2>
-                <?php
-                    echo $pageVars["data"]["current_report"]["feature_data"]["Report_Title"] ;
-                ?>
-            </h2>
-        </div>
-        <div class="fullRow">
-            <hr />
-            <h4> Report: </h4>
+    <div class="fullRow">
+        <h2>
             <?php
-                echo $pageVars["data"]["current_report"]["report_data"] ;
+                echo $pageVars["data"]["current_report"]["feature_data"]["Report_Title"] ;
             ?>
-        </div>
-        <div class="fullRow">
-            <hr />
-            <p class="text-center">
-                Visit <a href="http://www.pharaohtools.com">www.pharaohtools.com</a> for more
-            </p>
-        </div>
+        </h2>
+    </div>
+    <div class="fullRow">
+        <hr />
+        <h4> Report: </h4>
+        <?php
+            echo $pageVars["data"]["current_report"]["report_data"] ;
+        ?>
+    </div>
+    <div class="fullRow">
+        <hr />
+        <p class="text-center">
+            Visit <a href="http://www.pharaohtools.com">www.pharaohtools.com</a> for more
+        </p>
     </div>
 </div>
