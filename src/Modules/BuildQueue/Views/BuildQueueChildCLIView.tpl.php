c925b13a43216e23c85883706c5ed8be6158f6f2
David Amanshia <phpengine@pharaotools.com>
2017-03-27 23:30:17 +0100

    schedule to queue

--- src/Modules/BuildQueue/Views/BuildQueueChildCLIView.tpl.php
+++ src/Modules/BuildQueue/Views/BuildQueueChildCLIView.tpl.php
@@ -1,12 +1,12 @@
 <?php
 
-echo "Executing Scheduled Builds\n" ;
+echo "Executing Queued Builds\n" ;
 
-if (count($pageVars["data"]["scheduled"])>0) {
-    foreach ($pageVars["data"]["scheduled"] as $pipe) {
+if (count($pageVars["data"]["queued"])>0) {
+    foreach ($pageVars["data"]["queued"] as $pipe) {
         // echo $pipe["project-name"]."\n" ;
     }
     foreach ($pageVars["data"]["executions"] as $pipeTailSlug => $pipeTailDetails) {
         echo "Slug: ".$pipeTailSlug.", Name: ".$pipeTailDetails["name"].", Run ID ".$pipeTailDetails["result"]."\n" ; } }
 else {
-    echo "No Builds scheduled to run now\n" ; }
+    echo "No Builds queued to run now\n" ; }
