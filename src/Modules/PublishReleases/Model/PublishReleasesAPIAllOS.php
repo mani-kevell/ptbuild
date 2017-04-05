7d78f58e289c5934f12bacb35c51c0837f73734f
David Amanshia <phpengine@pharaotools.com>
2017-04-05 23:02:16 +0100

    publish releases api handling

--- src/Modules/PublishReleases/Model/PublishReleasesAPIAllOS.php
+++ src/Modules/PublishReleases/Model/PublishReleasesAPIAllOS.php
@@ -15,42 +15,49 @@ class PublishReleasesAPIAllOS extends Base {
     public $modelGroup = array("API") ;
 
     public function allowedFunctions() {
-        return array('get_status') ;
+        return array('get_releases') ;
     }
 
-    public function get_status() {
-
-        // load repo
+    public function get_releases() {
         $this->params['item'] = $this->params['slug'] ;
         $pbsf = new \Model\PublishReleases() ;
         $pbs = $pbsf->getModel($this->params) ;
         $pipeline = $pbs->getPipeline() ;
-        // if repo settings include
 
-        if ($pipeline['last_status'] == true) {
+        if ($pipeline['last_status'] === true) {
+
             $status_string = 'success' ;
             $run_id = $pipeline['last_success_build'] ;
             $run_time = $pipeline['last_success'] ;
-        } else if ($pipeline['last_status'] == false) {
+
+        } else if ($pipeline['last_status'] === false) {
+
             $status_string = 'failure' ;
             $run_id = $pipeline['last_fail_build'] ;
             $run_time = $pipeline['last_fail'] ;
-        } else if ($pipeline['last_status'] == null) {
+
+        } else if ($pipeline['last_status'] === null) {
+
             $status_string = 'unknown' ;
             $run_id = $pipeline['last_run_build'] ;
             $run_time = $pipeline['last_run_start'] ;
+
         }
 
         return array(
             'status' => $status_string,
             'run_id' => $run_id,
             'build_job_title' => $pipeline['project-name'],
-            'build_job_link' => 'http://'.$_SERVER["SERVER_NAME"].'/index.php?control=BuildHome&action=show&item='.$this->params['slug'],
-            'build_run_link' => 'http://'.$_SERVER["SERVER_NAME"].'/index.php?control=PipeRunner&action=summary&item=script_testing&run-id='.$run_id,
+            'build_job_link' =>
+                'http://'.$_SERVER["SERVER_NAME"].
+                '/index.php?control=BuildHome&action=show&item='.
+                $this->params['slug'],
+            'build_run_link' =>
+                'http://'.$_SERVER["SERVER_NAME"].
+                '/index.php?control=PipeRunner&action=summary&item=script_testing&run-id='.
+                $run_id,
             'build_run_time' => $run_time,
         ) ;
-
-
     }
 
 }
