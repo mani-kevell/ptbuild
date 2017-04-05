f692a38a22806c7eb372d5c139e9578f5eeec1a1
David Amanshia <phpengine@pharaotools.com>
2017-04-05 13:18:23 +0100

    report to status

--- src/Modules/PublishStatus/Views/PublishStatusHTMLView.tpl.php
+++ src/Modules/PublishStatus/Views/PublishStatusHTMLView.tpl.php
@@ -1,15 +1,14 @@
 <div class="container" id="wrapper">
     <div class="fullRow">
         <h2>
-            <?php
-                echo $pageVars["data"]["current_report"]["feature_data"]["Report_Title"] ;
-            ?>
+            Current Build Status
         </h2>
         <h4>
             <a href="index.php?control=BuildHome&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>">
                 Return To Build Home :
                 <strong>
                     <?php echo $pageVars["data"]["pipeline"]["project-name"] ; ?>
+                    <?php var_dump($pageVars["data"]["pipeline"]) ; ?>
                 </strong>
             </a>
         </h4>
@@ -17,7 +16,7 @@
     <div class="fullRow">
         <hr />
         <?php
-            echo $pageVars["data"]["current_report"]["report_data"] ;
+            echo $pageVars["data"]["current_status"]["status_data"] ;
         ?>
     </div>
     <div class="fullRow">
@@ -27,4 +26,4 @@
         </p>
     </div>
 </div>
-<link rel="stylesheet" type="text/css" href="/Assets/Modules/PublishStatus/css/publishhtmlreports.css">
+<link rel="stylesheet" type="text/css" href="/Assets/Modules/PublishStatus/css/publishstatus.css">
