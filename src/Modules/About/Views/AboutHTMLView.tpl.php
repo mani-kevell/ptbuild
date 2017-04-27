2fb65003350d6a406ca6107551d8f2d316524b14
Pharaoh Tools <phpengine@pharaohtools.com>
2017-04-27 02:30:54 +0100

    remove search from menu

--- src/Modules/About/Views/AboutHTMLView.tpl.php
+++ src/Modules/About/Views/AboutHTMLView.tpl.php
@@ -10,19 +10,9 @@
                 <div id="page_sidebar" class="navbar-default col-sm-2 sidebar" role="navigation">
                     <div class="sidebar-nav ">
                         <ul class="nav in" id="side-menu">
-                            <li class="sidebar-search">
-                                <div class="input-group custom-search-form hvr-bounce-in">
-                                    <input type="text" class="form-control" placeholder="Search...">
-                                    <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                                </div>
-                                <!-- /input-group -->
-                            </li>
                             <li>
-                                <a href="/index.php?control=Index&action=show" class="active hvr-bounce-in"><i class="fa fa-dashboard fa-fw hvr-grow-shadow"></i> Dashboard</a>
+                                <a href="/index.php?control=Index&action=show" class="active hvr-bounce-in">
+                                    <i class="fa fa-dashboard fa-fw hvr-grow-shadow"></i> Dashboard</a>
                             </li>
                         </ul>
                     </div>
