0f0f9681a3a2267f5db680c42b360f48dba7b2e7
Pharaoh Tools <phpengine@pharaohtools.com>
2017-04-27 02:32:39 +0100

    id fix

--- src/Modules/Workspace/Views/WorkspaceHTMLView.tpl.php
+++ src/Modules/Workspace/Views/WorkspaceHTMLView.tpl.php
@@ -15,7 +15,7 @@
                 ?>
                 <h3><?= $stat; ?> Pipeline <?php echo $pageVars["data"]["pipeline"]["project-name"] ; ?></h3>
 
-                            <div class="navbar-default col-sm-2 sidebar" role="navigation">
+                            <div id="page_sidebar" class="navbar-default col-sm-2 sidebar" role="navigation">
                                 <div class="sidebar-nav ">
                                     <div class="sidebar-search">
                                         <button class="btn btn-success" id="menu_visibility_label" type="button">
@@ -24,16 +24,6 @@
                                         <i class="fa fa-1x fa-toggle-off hvr-grow" id="menu_visibility_switch"></i>
                                     </div>
                                     <ul class="nav in" id="side-menu">
-                                        <li class="sidebar-search">
-                                            <div class="input-group custom-search-form hvr-bounce-in">
-                                                <input type="text" class="form-control" placeholder="Search...">
-                                                <span class="input-group-btn">
-							<button class="btn btn-default" type="button">
-								<i class="fa fa-search"></i>
-							</button>
-                        </span>
-                                            </div>
-                                        </li>
                                         <li>
                                             <a href="/index.php?control=Index&amp;action=show"class="hvr-bounce-in">
                                                 <i class="fa fa-dashboard hvr-bounce-in"></i> Dashboard
