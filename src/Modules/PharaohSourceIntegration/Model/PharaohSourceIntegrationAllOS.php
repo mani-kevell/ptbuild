b2b9fe29f1a6eb710f03e513a3c758c09032e32e
Pharaoh Tools <phpengine@pharaohtools.com>
2017-05-10 09:41:38 +0100

    Pharaoh Source integration further work to create repo in API

--- src/Modules/PharaohSourceIntegration/Model/PharaohSourceIntegrationAllOS.php
+++ src/Modules/PharaohSourceIntegration/Model/PharaohSourceIntegrationAllOS.php
@@ -76,7 +76,8 @@ class PharaohSourceIntegrationAllOS extends Base {
         $source_max = 1;
         for ($i=0; $i<=$source_max; $i++) {
             if (isset($settings['PharaohSourceIntegration']['source_instance_url_'.$i])) {
-                $servers[] = $settings['PharaohSourceIntegration']['source_instance_url_'.$i] ;
+                $s = $settings['PharaohSourceIntegration']['source_instance_url_'.$i] ;
+                $servers[$s] = $s ;
             }
         }
         return $servers;
@@ -87,23 +88,67 @@ class PharaohSourceIntegrationAllOS extends Base {
         return $settings ;
     }
 
-    public function executeStep($step) {
+    public function executeStep($step, $item, $hash) {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
         if ( $step["steptype"] == "create_repository") {
             $logging->log("Running Creation of a Repository in Pharaoh Source...", $this->getModuleName()) ;
-            $logging->log("Calling API to {$step['server_url']}...", $this->getModuleName()) ;
-            return true ; }
+
+            $pf = new \Model\Pipeline() ;
+            $pipelineBase = $pf->getModel($this->params) ;
+            $pipelineInstance = $pipelineBase->getPipeline($item) ;
+
+            $server_url = $pipelineInstance['steps'][$hash]["server_url"] ;
+            $logging->log("Calling API to {$server_url}...", $this->getModuleName()) ;
+            $res = $this->createRepository($server_url, $pipelineInstance['steps'][$hash]["repository_name"]) ;
+
+            return $res ; }
         else {
             $logging->log("Unrecognised Build Step Type {$step["type"]} specified in Shell Module", $this->getModuleName()) ;
             return false ; }
     }
 
-    protected function createRepository($server, $repo_slug) {
-        $settings = \Model\AppConfig::getAppVariable("mod_config");
-        return $settings ;
+    protected function createRepository($server_url, $repo_name) {
+        // load repo
+        $this->params['item'] = $this->params['slug'] ;
+
+        $apif = new \Model\PharaohAPI();
+        $params = $this->params ;
+        $params['api_module'] = 'RepositoryConfigure' ;
+        $params['api_function'] = 'create_repository' ;
+        $params['api_instance_url'] = $server_url ;
+        $params['api_key'] = $this->findInstanceKey($server_url) ;
+        $params['api_param_repo_name'] = $repo_name ;
+
+        $api_request = $apif->getModel($params, 'Request') ;
+        $result = $api_request->performAPIRequest() ;
+
+        return $result;
+
     }
 
 
+    public function findInstanceKey($instance_url) {
+        $instance_url = $this->ensureTrailingSlash($instance_url) ;
+        $settings = $this->getSettings() ;
+        $instance_key = false ;
+        if ($settings['PharaohBuildIntegration']['enabled'] === 'on') {
+            for ($i=0; $i<5; $i++) {
+                if (isset($settings['PharaohBuildIntegration']['build_instance_url_'.$i])) {
+                    $url = $settings['PharaohBuildIntegration']['build_instance_url_'.$i] ;
+                    $url_with_slash = $this->ensureTrailingSlash($url) ;
+                    if ($url_with_slash === $instance_url) {
+                        $instance_key = $settings['PharaohBuildIntegration']['build_instance_key_'.$i] ;
+                    }
+                }
+            }
+        }
+        else {
+            // Build Integration is not enabled
+            return false ;
+        }
+        return $instance_key ;
+
+    }
 
 }
