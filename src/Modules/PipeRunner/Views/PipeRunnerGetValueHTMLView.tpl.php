254fd66f4ad656eb8a4168a9fac7f79956b71621
Pharaoh Tools <phpengine@pharaohtools.com>
2017-05-21 23:48:33 +0100

    pipe runnner parameters menu in interface display

--- src/Modules/PipeRunner/Views/PipeRunnerGetValueHTMLView.tpl.php
+++ src/Modules/PipeRunner/Views/PipeRunnerGetValueHTMLView.tpl.php
@@ -1,55 +1,63 @@
 <div class="container" id="wrapper">
-    <div class="navbar-default col-sm-2 sidebar" role="navigation">
-        <div class="sidebar-nav ">
-            <ul class="nav in" id="side-menu">
-                <li class="sidebar-search">
-                    <div class="input-group custom-search-form  hvr-bounce-in">
-                        <input type="text" class="form-control" placeholder="Search...">
-						<span class="input-group-btn">
-							<button class="btn btn-default" type="button">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </span>
-                    </div>
-                </li>
-                <li>
-                    <a href="/index.php?control=Index&action=show" class="hvr-bounce-in">
-                        <i class="fa fa-dashboard fa-fw hvr-bounce-in"></i> Dashboard
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?control=BuildHome&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>" class="hvr-bounce-in">
-                        <i class="fa fa-home fa-fw hvr-bounce-in"></i>  Pipeline Home
-                    </a>
-                </li>
-                <li>
-                    <a href="/index.php?control=BuildList&action=show" class="hvr-bounce-in">
-                        <i class="fa fa-bars fa-fw hvr-bounce-in"></i> All Pipelines
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?control=Workspace&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
-                        <i class="fa fa-folder-open-o hvr-bounce-in"></i> Workspace
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?control=BuildMonitor&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
-                        <i class="fa fa-bar-chart-o hvr-bounce-in"></i> Monitors
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?control=PipeRunner&action=history&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
-                        <i class="fa fa-history fa-fw hvr-bounce-in"></i> History <span class="badge"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?control=PipeRunner&action=start&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
-                        <i class="fa fa-sign-in fa-fw hvr-bounce-in"></i> Run Again
-                    </a>
-                </li>
-            </ul>
+
+    <div id="page_content" class="col-lg-12 well well-lg">
+        <div id="page_sidebar" class="navbar-default col-sm-2 sidebar" role="navigation">
+            <div class="sidebar-nav ">
+                <div class="sidebar-search">
+                    <button class="btn btn-success" id="menu_visibility_label" type="button">
+                        Show Menu
+                    </button>
+                    <i class="fa fa-1x fa-toggle-off hvr-grow" id="menu_visibility_switch"></i>
+                </div>
+                <ul class="nav in" id="side-menu">
+                    <li class="sidebar-search">
+                        <div class="input-group custom-search-form  hvr-bounce-in">
+                            <input type="text" class="form-control" placeholder="Search...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/index.php?control=Index&action=show" class="hvr-bounce-in">
+                            <i class="fa fa-dashboard fa-fw hvr-bounce-in"></i> Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php?control=BuildHome&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>" class="hvr-bounce-in">
+                            <i class="fa fa-home fa-fw hvr-bounce-in"></i>  Pipeline Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/index.php?control=BuildList&action=show" class="hvr-bounce-in">
+                            <i class="fa fa-bars fa-fw hvr-bounce-in"></i> All Pipelines
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php?control=Workspace&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
+                            <i class="fa fa-folder-open-o hvr-bounce-in"></i> Workspace
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php?control=BuildMonitor&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
+                            <i class="fa fa-bar-chart-o hvr-bounce-in"></i> Monitors
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php?control=PipeRunner&action=history&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
+                            <i class="fa fa-history fa-fw hvr-bounce-in"></i> History <span class="badge"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php?control=PipeRunner&action=start&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"  class="hvr-bounce-in">
+                            <i class="fa fa-sign-in fa-fw hvr-bounce-in"></i> Run Again
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
 
     <?php $act= "index.php?control=PipeRunner&action=start&item={$pageVars["data"]["pipeline"]["project-slug"]}" ; ?>
 
@@ -68,88 +76,96 @@
                 </div>
 
                 <div class="form-group">
-                    <div class="col-sm-10">
+                    <div class="col-sm-12">
 
                         <?php
 
                         foreach ($pageVars["data"]["pipeline"]["settings"]["PipeRunParameters"]["parameters"] as $parameter) {
 
-//                            var_dump($parameter) ;
+                            ?>
 
-                            if ($parameter["param_type"]=="text") {
-                                ?>
-
-                            <div class="col-sm-4">
-                                <label for="build-parameters[<?php echo $parameter["param_name"] ; ?>]" class="control-label text-left"><?php echo $parameter["param_name"] ; ?></label>
-                            </div>
-
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]" value="<?php echo $parameter["param_default"] ; ?>" />
-                            </div>
+                            <div class="col-sm-12">
 
                                 <?php
-                            }
 
-                            else if ($parameter["param_type"]=="textarea") {
-                                ?>
-
-                                <div class="col-sm-4">
-                                    <label for="build-parameters[<?php echo $parameter["param_name"] ; ?>]" class="control-label text-left"><?php echo $parameter["param_name"] ; ?></label>
-                                </div>
-
-                                <div class="col-sm-8">
-                                    <textarea class="form-control col-sm-7" name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]"><?php echo $parameter["param_default"] ; ?></textarea>
-                                </div>
-
-                            <?php
-                            }
-
-                            else if ($parameter["param_type"]=="options") {
-
-//                              var_dump("parameter", $parameter) ;
-
-                                ?>
+                                if ($parameter["param_type"]=="text") {
+                                    ?>
 
                                 <div class="col-sm-4">
                                     <label for="build-parameters[<?php echo $parameter["param_name"] ; ?>]" class="control-label text-left"><?php echo $parameter["param_name"] ; ?></label>
                                 </div>
 
                                 <div class="col-sm-8">
-
-                                    <select name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]">
-                                        <?php
-                                            $original_options = explode("\n", $parameter["param_options"]) ;
-                                            foreach ($original_options as $option_value) {
-                                                $option_value = rtrim($option_value) ;
-//                                                var_dump($option_value, $parameter["param_default"]) ;
-                                                if ($option_value == $parameter["param_default"]) { $selstring = " selected='selected' " ; }
-                                                else { $selstring = "" ; }
-                                                echo '<option '.$selstring.' value="'.$option_value.'">'.$option_value.'</option>' ; }
-                                        ?>
-                                    </select>
-
+                                    <input type="text" class="form-control" name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]" value="<?php echo $parameter["param_default"] ; ?>" />
                                 </div>
 
-                            <?php
-                            }
+                                    <?php
+                                }
 
-                            else if ($parameter["param_type"]=="boolean") {
+                                else if ($parameter["param_type"]=="textarea") {
+                                    ?>
+
+                                    <div class="col-sm-4">
+                                        <label for="build-parameters[<?php echo $parameter["param_name"] ; ?>]" class="control-label text-left"><?php echo $parameter["param_name"] ; ?></label>
+                                    </div>
+
+                                    <div class="col-sm-8">
+                                        <textarea class="form-control col-sm-7" name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]"><?php echo $parameter["param_default"] ; ?></textarea>
+                                    </div>
+
+                                <?php
+                                }
+
+                                else if ($parameter["param_type"]=="options") {
+
+    //                              var_dump("parameter", $parameter) ;
+
+                                    ?>
+
+                                    <div class="col-sm-4">
+                                        <label for="build-parameters[<?php echo $parameter["param_name"] ; ?>]" class="control-label text-left"><?php echo $parameter["param_name"] ; ?></label>
+                                    </div>
+
+                                    <div class="col-sm-8">
+
+                                        <select name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]">
+                                            <?php
+                                                $original_options = explode("\n", $parameter["param_options"]) ;
+                                                foreach ($original_options as $option_value) {
+                                                    $option_value = rtrim($option_value) ;
+    //                                                var_dump($option_value, $parameter["param_default"]) ;
+                                                    if ($option_value == $parameter["param_default"]) { $selstring = " selected='selected' " ; }
+                                                    else { $selstring = "" ; }
+                                                    echo '<option '.$selstring.' value="'.$option_value.'">'.$option_value.'</option>' ; }
+                                            ?>
+                                        </select>
+
+                                    </div>
+
+                                <?php
+                                }
+
+                                else if ($parameter["param_type"]=="boolean") {
+                                    ?>
+
+                                    <div class="col-sm-4">
+                                        <label for="build-parameters[<?php echo $parameter["param_name"] ; ?>]" class="control-label text-right"><?php echo $parameter["param_name"] ; ?></label>
+                                    </div>
+
+                                    <div class="col-sm-8">
+                                        <input type="checkbox" class="text-left form-control" name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]" value="<?php echo $parameter["param_boolean_default"] ; ?>" />
+                                    </div>
+
+
+                                <?php
+                                }
+
+
                                 ?>
 
-                                <div class="col-sm-4">
-                                    <label for="build-parameters[<?php echo $parameter["param_name"] ; ?>]" class="control-label text-right"><?php echo $parameter["param_name"] ; ?></label>
-                                </div>
+                            </div>
 
-                                <div class="col-sm-8">
-                                    <input type="checkbox" class="text-left form-control" name="build-parameters[<?php echo $parameter["param_name"] ; ?>]" id="build-parameters[<?php echo $parameter["param_name"] ; ?>]" value="<?php echo $parameter["param_boolean_default"] ; ?>" />
-                                </div>
-
-
-                            <?php
-                            }
-
-                            ?>
-                            <?php
+                        <?php
                         }
 
                         ?>
@@ -189,4 +205,5 @@
 
         </div>
     </form>
+</div><!-- /.container -->
 </div><!-- /.container -->
