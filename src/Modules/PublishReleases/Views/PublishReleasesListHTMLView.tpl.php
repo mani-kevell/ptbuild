bf7f17f7a4df2aa1b2ba87f6042c8930ffcc37c5
David Amanshia <phpengine@pharaohtools.com>
2017-08-03 21:14:52 +0100

    formatting and removed useless text

--- src/Modules/PublishReleases/Views/PublishReleasesListHTMLView.tpl.php
+++ src/Modules/PublishReleases/Views/PublishReleasesListHTMLView.tpl.php
@@ -49,58 +49,12 @@ if ($pageVars["route"]["action"] === 'image') {
 
     </div>
     <div class="col-lg-12">
-        <hr>
-        <h4>
-            Permalink for Status Image
-        </h4>
 
-        <?php
-        if ($pageVars['data']['is_https'] === true) {
-            $proto = 'https://' ;
-        } else {
-            $proto = 'http://' ;
-        }
-
-        $url = $proto ;
-        $url .= $_SERVER['HTTP_HOST'] ;
-        $url .= '/index.php?control=PublishReleases&action=image&item=' ;
-        $url .= $pageVars["data"]["pipeline"]['project-slug'] ;
-
-        ?>
-
-        <h3>
-            <a href="<?php echo $url ; ?>"
-               target="_blank"
-               class="image_permalink"><?php echo $url ; ?></a>
-        </h3>
-
-        <?php
-
-        $mn = 'PublishReleases' ;
-        if (isset($steps[$mn]["enabled"]) && $steps[$mn]["enabled"] === "on") {
-            foreach ($pageVars["data"]["status_list"]["statuses"] as $statusHash => $statusDetail) {
-
-                $dir = $statusDetail["Report_Directory"];
-                if (substr($dir, -1) !== DS) {
-                    $dir = $dir . DS ; }
-                $indexFile = $statusDetail["Index_Page"];
-                $statusTitle = $statusDetail["Report_Title"];
-
-                echo '<a href="index.php?control=PublishReleases&action=status&item=' ;
-                echo $pageVars["data"]["pipeline"]["project-slug"].'">' ;
-                echo '  '.$statusTitle ;
-                echo '</a>' ;
-
-            } }
-
-        ?>
-
-        <h3>
+        <h3 class="text-center">
             Releases List
         </h3>
 
         <?php
-
 
         $mn = 'PublishReleases' ;
         foreach ($pageVars["data"]["releases_list"]["custom_release"] as $releaseHash => $releaseDetail) {
@@ -110,7 +64,9 @@ if ($pageVars["route"]["action"] === 'image') {
             <div class="col-sm-12 one_release_type">
 
                 <div class="col-sm-12">
-                    <h2><?php echo $releaseDetail['release_title'] ; ?></h2>
+                    <h2 class="text-center">
+                        <?php echo $releaseDetail['release_title'] ; ?>
+                    </h2>
                 </div>
 
                 <?php
