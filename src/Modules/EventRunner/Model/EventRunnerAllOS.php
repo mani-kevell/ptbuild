cd9de1a03ec25ee146ff2831a2f491d54e64df23
David Amanshia <phpengine@pharaohtools.com>
2018-07-30 11:50:35 +0100

    hide event announcement

--- src/Modules/EventRunner/Model/EventRunnerAllOS.php
+++ src/Modules/EventRunner/Model/EventRunnerAllOS.php
@@ -20,9 +20,9 @@ class EventRunnerAllOS extends BaseLinuxApp {
     }
 
     public function eventRunner($event, $allResults=false) {
-        $loggingFactory = new \Model\Logging();
-        $logging = $loggingFactory->getModel($this->params);
-        $logging->log("Executing event $event", $this->getModuleName()) ;
+//        $loggingFactory = new \Model\Logging();
+//        $logging = $loggingFactory->getModel($this->params);
+//        $logging->log("Executing event $event", $this->getModuleName()) ;
         $this->params["app-settings"]["app_config"] = \Model\AppConfig::getAppVariable("app_config");
         $this->params["app-settings"]["mod_config"] = \Model\AppConfig::getAppVariable("mod_config");
 //         $logging->log("Loading application settings during event $event", $this->getModuleName()) ;
