228fec414af52da7a478ea05c41ecda741ed3622
David Amanshia <phpengine@pharaohtools.com>
2018-08-01 00:09:08 +0100

    vars generic

--- vars/default.php
+++ vars/default.php
@@ -1,12 +1,13 @@
 <?php
 
 $variables = array() ;
-$variables['application_slug'] = 'ptbuild' ;
-$variables['description'] = 'Pharaoh Build Development VM' ;
-$variables['subdomain'] = 'build' ;
-$variables['webclientsubdomain'] = 'build' ;
-$variables['server_subdomain'] = 'server' ;
+$variables['application_slug'] = 'build' ;
+$variables['random_port_suffix'] = '57' ;
 $variables['domain'] = 'pharaohtools.vm' ;
+$variables['full_slug'] = 'pt'.$variables['application_slug'] ;
+$variables['description'] = 'Pharaoh '.ucfirst($variables['application_slug']).' Development VM' ;
+$variables['subdomain'] = $variables['application_slug'] ;
+$variables['webclientsubdomain'] = $variables['application_slug'] ;
+$variables['server_subdomain'] = 'server' ;
 $variables['friendly_app_slug'] = $variables['application_slug'] ;
 $variables['desktop_app_slug'] = $variables['friendly_app_slug'] ;
-$variables['random_port_suffix'] = '58' ;
